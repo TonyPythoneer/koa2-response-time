848efbd8af6bd4357213d6a2ad0f230b19732002
tony <tonypythoneer@gmail.com>
2017-01-10 12:26:44 +0800

    Built main module

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as Koa from "koa";
+
+
+/**
+ * Add X-Response-Time header field.
+ *
+ * @return {Function} It's a middleware for koa2
+ */
+export function responseTime(): Koa.Middleware {
+    return async function(ctx, next) {
+        const start = Date.now();
+        await next();
+        const delta = Math.ceil(Date.now() - start);
+        this.set("X-Response-Time", `${delta} ms`);
+    };
+};
+export default responseTime;
