b450cfb63e66361daa5725c49b488e6134654d02
tony <tonypythoneer@gmail.com>
2017-01-10 12:28:01 +0800

    Add tests

---
+++ src/tests.ts
@@ -0,0 +1,17 @@
+import test from "ava";
+import * as Koa from "koa";
+
+const server = new Koa();
+
+
+/////////////////////
+// globals         //
+/////////////////////
+
+
+///////////////////
+// tests         //
+///////////////////
+test("SetUp", t => {
+
+});
