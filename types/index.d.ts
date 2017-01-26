becea4dd0e078acf5d679e8c36c2ac7dd7806bcc
TonyPythoneer <tonypythoneer@gmail.com>
2017-01-26 13:35:45 +0800

    Making Publish version

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="koa" />
-import * as Koa from "koa";
-/**
- * Add X-Response-Time header field.
- *
- * @return {Function} It's a middleware for koa2
- */
-export declare function responseTime(): Koa.Middleware;
-export default responseTime;
